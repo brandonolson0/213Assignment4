598da9509fbe8e3491b4cb3bde080e55362dd611
karinawhaley <karina.whaley@ndsu.edu>
2023-11-09 13:04:27 -0600

    Added Logon page
    Created a basic logon page, I will update it for when we have members and instructors.

---
+++ Assignment4/Logon.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment4
+{
+    public partial class Logon : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string userName = Login1.UserName;
+            string pass = Login1.Password;
+
+
+
+
+            if (userName == "cat" && pass == "cat")
+            {
+
+                FormsAuthentication.RedirectFromLoginPage(userName, true);
+
+
+            }
+            else
+                Response.Redirect("Logon.aspx", true);
+
+        }
+    }
+}
--- Assignment4/Logon.aspx.designer.cs
+++ Assignment4/Logon.aspx.designer.cs
@@ -11,16 +11,16 @@ namespace Assignment4
 {
 
 
-    public partial class SiteMaster
+    public partial class Logon
     {
 
         /// <summary>
-        /// MainContent control.
+        /// Login1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.Login Login1;
     }
 }
